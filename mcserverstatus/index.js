2dc4de39db3ff4e9265b5e48763b55055bbac222
Nate314 <NathanGawith@gmail.com>
2022-08-09 20:42:57 -0500

    fix no one crafting query

--- mcserverstatus/index.js
+++ mcserverstatus/index.js
@@ -81,7 +81,7 @@ if (isAllDefined([ params.serverName, params.serverAddress ])) {
       hostName: params.serverAddress,
       isServerOnline: params.isServerOnline === 'true',
       serverDescription: params.serverDescription,
-      onlineUsernameList: params.onlineUsernameList.includes(',') ? params.onlineUsernameList.split(',') : [],
+      onlineUsernameList: params.onlineUsernameList.length ? params.onlineUsernameList.split(',') : [],
     });
   } else {
     console.log('retrieving server info');
